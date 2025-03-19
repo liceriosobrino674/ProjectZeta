7953f1774f92fa0e4eadf33e05a1ece356f5c788
liceriosobrino674 <liceriosobrino674@gmail.com>
2025-03-19 07:47:02 +0000

    Add 14.rs

---
+++ src/14.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let x = 5;
+    if x == 3 {
+        println!("Three");
+    } else if x == 5 {
+        println!("Five");
+    } else {
+        println!("Neither three nor five");
+    }
+}
