94d30f42e7db0aa1dff7560241aeb14fe8de26e8
liceriosobrino674 <liceriosobrino674@gmail.com>
2025-04-11 12:26:21 +0000

    Add 32.rs

---
+++ src/32.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // Define a variable to hold a string
+    let mut str = String::from("Hello, world!");
+
+    // Add an element to the end of the string
+    str.push_str(", ");
+
+    // Print the modified string
+    println!("{}", str);
+}
