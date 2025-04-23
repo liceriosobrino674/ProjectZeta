ae10fd6d03d7c38d3267d8a3525395eab9e7858a
liceriosobrino674 <liceriosobrino674@gmail.com>
2025-04-23 10:21:21 +0000

    Add 59.rs

---
+++ src/59.rs
@@ -0,0 +1,11 @@
+// This Rust code is designed to demonstrate basic syntax and control structures commonly used in programming.
+// It contains variables, functions, loops, and conditionals.
+
+fn main() {
+    let x = 5;
+    if x > 0 {
+        println!("The value of x is positive.");
+    } else {
+        println!("The value of x is not positive.");
+    }
+}
